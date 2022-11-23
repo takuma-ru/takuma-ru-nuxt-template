c4efafb6b9dc0ac0dc5fe92d81e2dc1147908e42
takuma-ru <kcattdev@gmail.com>
2022-11-23 20:05:36 +0900

    [Add] setup

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW()
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,155 @@
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+
+  modules: [
+    '@vueuse/nuxt'
+  ],
+
+  components: {
+    dirs: [
+      'components/layouts',
+      'components/utils'
+    ]
+  },
+
+  imports: {
+    dirs: [
+      'composables',
+      'composables/*/+.{ts,js,mjs,mts}',
+      'utils',
+      'utils/*/*.{ts,js,mjs,mts}'
+    ]
+  },
+
+  app: {
+    head: {
+      title: 'IoT Compost Toilet',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: '' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/icon_rounded.png' },
+        { rel: 'apple-touch-icon', href: '/apple_touch_icon.png', sizes: '180x180' },
+        { rel: 'mask-icon', color: '#FCFCF9', href: '/mask_icon.svg' },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap'
+        },
+        {
+          rel: 'manifest',
+          'data-n-head': '1',
+          'data-h-id': 'manifest',
+          href: 'manifest.webmanifest'
+        }
+      ]
+    }
+  },
+
+  vite: {
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        injectRegister: 'auto',
+        devOptions: {
+          enabled: false
+        },
+        workbox: {
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'google-fonts-cache',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24 * 30 // <== 30 days
+                },
+                cacheableResponse: {
+                  statuses: [ 0, 200 ]
+                }
+              }
+            },
+            {
+              urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'gstatic-fonts-cache',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24 * 30 // <== 30 days
+                },
+                cacheableResponse: {
+                  statuses: [ 0, 200 ]
+                }
+              }
+            }
+          ]
+        },
+        includeAssets: [ 'icon_rounded.png', 'apple_touch_icon.png', 'masked_icon.svg' ],
+        manifest: {
+          lang: 'ja',
+          name: 'IoT Compost Toilet',
+          short_name: 'ComToile',
+          description: '',
+          start_url: '/',
+          display: 'standalone',
+          background_color: '#FCFCF9',
+          theme_color: '#FCFCF9',
+          icons: [
+            {
+              src: '/icons/icon_64.png',
+              type: 'image/png',
+              sizes: '64x64',
+              purpose: 'any'
+            },
+            {
+              src: '/icons/icon_120.png',
+              type: 'image/png',
+              sizes: '120x120',
+              purpose: 'any'
+            },
+            {
+              src: '/icons/icon_144.png',
+              type: 'image/png',
+              sizes: '144x144',
+              purpose: 'any'
+            },
+            {
+              src: '/icons/icon_152.png',
+              type: 'image/png',
+              sizes: '152x152',
+              purpose: 'any'
+            },
+            {
+              src: '/icons/icon_192.png',
+              type: 'image/png',
+              sizes: '192x192',
+              purpose: 'any'
+            },
+            {
+              src: '/icons/icon_384.png',
+              type: 'image/png',
+              sizes: '384x384',
+              purpose: 'any'
+            },
+            {
+              src: '/icons/icon_512.png',
+              type: 'image/png',
+              sizes: '512x512',
+              purpose: 'any'
+            }
+          ]
+        }
+      })
+    ]
+  }
+})
